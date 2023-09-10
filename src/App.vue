e2a67ee86456b36580bb951e017b75129f7f9572
alankritkadian <alankritkadian@gmail.com>
2023-09-10 20:50:51 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <JobApply/>
+</template>
+
+<script>
+import JobApply from './components/JobApply.vue';
+
+export default {
+  name: 'App',
+  components: { JobApply }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
